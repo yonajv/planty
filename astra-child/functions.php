e7f0906d1d27596b2873fb7183041d5b6c03c478
Myona Jovicic <myona.jovicic@gmail.com>
2024-04-10 17:43:16 +0200

    first commit header and footer added

--- astra-child/functions.php
+++ astra-child/functions.php
@@ -2,5 +2,21 @@
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
 {
-	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+	wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css');
 }
+
+
+
+  add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
+  function add_extra_item_to_nav_menu( $items, $args ) {
+	  if (is_user_logged_in() && $args->theme_location == 'primary') {
+		$items .= '<li class = "admin_button"><a class="admin_a" href="' . admin_url() . '">Admin</a></li>';
+    }
+	return $items;
+    }
+	
+
+
+	
+
+?>
